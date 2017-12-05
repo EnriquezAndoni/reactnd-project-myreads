7a88e2a4c20da4044fcf4d94e5ad9662e0f9853f
EnriquezAndoni <github@andonienriquez.com>
2017-12-05 14:36:36 +0100

    fix: Remove broken elements

--- src/App.js
+++ src/App.js
@@ -15,21 +15,7 @@ class BooksApp extends React.Component {
       this.setState({books})
     }))
   }
-
-  updateBookControl = () => {
-    this.setState((prevState) => ({
-      // books: prevState.books.filter((b) => b.
-    }))
-  }
-
-  removeContact = (contact) => {
-    this.setState((prevState) => ({
-      contacts: prevState.contacts.filter((c) => c.id !== contact.id)
-    }))
-
-    ContactsAPI.remove(contact)
-  }
-
+  
   render() {
     return(
       <div className='app'>
